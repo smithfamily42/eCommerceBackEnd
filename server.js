de6bf9808c3ae960b97d03d641fbf61f3c0469a4
Brad Smith <smithfamily42@gmail.com>
2021-06-20 21:59:31 -0700

    seeded the data after changing server.js, completing routes, and completing models

--- server.js
+++ server.js
@@ -9,13 +9,6 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const routes = require('./routes');
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
